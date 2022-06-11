1603f2dd5ae29d9080c19c6be6fe5010ad2674dc
Kenny50 <kennethchangla@gmail.com>
2022-06-11 14:18:54 +0800

    [ doc ] UiAction, UiState
    Author: @Kenny50

--- app/src/main/java/com/kenny/githubreposearch/data/remote/paging_source/UiAction.kt
+++ app/src/main/java/com/kenny/githubreposearch/data/remote/paging_source/UiAction.kt
@@ -1,6 +1,28 @@
 package com.kenny.githubreposearch.data.remote.paging_source
 
+import androidx.annotation.Keep
+
+/**
+ * Ui action to trigger viewModel callback
+ */
+@Keep
 sealed class UiAction {
+
+    /**
+     * Action to search
+     *
+     * while this action send to shareFlow via callback, the consumer of that flow will trigger search request
+     * @property query
+     */
+    @Keep
     data class Search(val query: String) : UiAction()
+
+    /**
+     * Action on scroll
+     *
+     * while this action send to shareFlow via callback, the consumer of that flow will produce scroll
+     * @property currentQuery
+     */
+    @Keep
     data class Scroll(val currentQuery: String) : UiAction()
 }
